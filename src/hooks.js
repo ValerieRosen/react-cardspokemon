c306f256114d35e126d68617a69b3ce73ea8d4e3
Valerie R <vrosenhauer@gmail.com>
2024-01-24 12:21:33 -0500

    added useAxios hooks

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 // business logic for flipping any type of card
 
@@ -8,8 +9,23 @@ function useFlip(currentFlip = true) {
   const flip = () => {
     setFlipped((isUp) => !isUp);
   };
-
   return [isFlipped, flip];
 }
 
-export { useFlip };
+function useAxios(baseUrl) {
+  const [responses, setResponses] = useState();
+
+  const addResponseData = async (
+    formatter = (data) => data,
+    restOfUrl = ""
+  ) => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses((data) => [...data, formatter(response.data)]);
+  };
+
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses([])];
+}
+
+export { useFlip, useAxios };
